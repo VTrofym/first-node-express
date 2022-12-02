33b3091820549384f5f9a96c82c4655b1799eebc
VTrofym <v.trof@icloud.com>
2022-12-02 14:05:14 +0200

    start

--- simple-express/app.js
+++ simple-express/app.js
@@ -1,38 +1,43 @@
+// сначала подключаются все сторонноие пакеты
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+// подключение роутов
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
+//создается экземпляр приложения
 const app = express();
 
-// view engine setup
+// подключаем шаблоны
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//блок подключения промежуточного ПО
+//порядок подключаемого промежуточного ПО имеет значение
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); //обработка статических ресурсов
 
+//Подключение роутеров в приложение
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
+// Вначале происходит обработка несуществующего роута или ошибка 404
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// мы создаем ошибку и пробрасываем ее дальше для обработки.
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render("error");
